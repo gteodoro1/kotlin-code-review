befdffa741f7499885d947262513dc0d746dad9b
gteodoro1 <guillemteodoro@gmail.com>
2026-07-25 18:13:32 +0200

    TechTest 1: Fix booting errors

--- src/main/kotlin/schwarz/jobs/interview/coupon/CouponApplication.kt
+++ src/main/kotlin/schwarz/jobs/interview/coupon/CouponApplication.kt
@@ -1,12 +1,10 @@
 package schwarz.jobs.interview.coupon
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
 
-@Configuration
-@ComponentScan
-//@SpringBootApplication
+//FIXED: uncomment @SpringBootApplication and remove redundant tags
+@SpringBootApplication
 class CouponApplication
 
 fun main(args: Array<String>) {
